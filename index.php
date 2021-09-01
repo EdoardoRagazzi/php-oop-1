7b455be5a90d9f2307ad426c80f7b0891295b751
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-09-01 17:10:21 +0200

    Construct / setRatings

--- index.php
+++ index.php
@@ -3,21 +3,41 @@
 class Movie {
     public $title;
     public $director;
-    public $ratings;
+    public $ratings = 0;
     public $boxoffice;
 
-}
+    function __construct($_title,$_director,$boxoffice){
+        $this->title = $_title;
+        $this->director = $_director,
+        $this->boxoffice = $_boxoffice,
+    }
+
+    public function setRatings($voto){
+        if( $voto > 8 ){
+            $this->ratings = 5;
+        } elseif ( $voto > 6){
+            $this->ratings = 4;
+        } elseif ($voto > 4){
+            $this->ratings = 3;
+        } elseif ( $voto>2){
+            $this->ratings = 2;
+        } else {
+            $this->ratings = 1;
+        }
+        return $this->ratings;
+    }
+
+};
 
 
-$filmAmericani = new Movie();
 
-
-$filmAmericani->title = 'Mr. & Mrs. Smith';
-$filmAmericani->director = 'Doug Liman';
-$filmAmericani->ratings = 6.5;
-$filmAmericani->boxoffice = 487.000;   //error if i try to icrement the zero like (487.000.000) !!!
+$filmAmericani = new Movie('Mr. & Mrs. Smith','Doug Liman',487.000);   //error if i try to icrement the zero like (487.000.000) !!!
 var_dump($filmAmericani);
 
+$filmAmericani->setRatings(5);
+var_dump($filmAmericani);
+
+echo $ratings;
 
 
 ?>
