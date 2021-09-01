7570496220570f22e7b21f79b24f94aac27abc08
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-09-01 16:32:58 +0200

    Variabili d'istanza

--- index.php
+++ index.php
@@ -10,7 +10,14 @@ class Movie {
 
 
 $filmAmericani = new Movie();
-var_dump($filmAmericani)
+
+
+$filmAmericani->title = 'Mr. & Mrs. Smith';
+$filmAmericani->director = 'Doug Liman';
+$filmAmericani->ratings = 6.5;
+$filmAmericani->boxoffice = 487.000;   //error if i try to icrement the zero like (487.000.000) !!!
+var_dump($filmAmericani);
+
 
 
 ?>
