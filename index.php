d243bccdcbfd285bb618856967f8df59affc886b
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-09-01 16:17:46 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+class Movie {
+    public $title;
+    public $director;
+    public $ratings;
+    public $boxoffice;
+
+}
+
+
+$filmAmericani = new Movie();
+var_dump($filmAmericani)
+
+
+?>
